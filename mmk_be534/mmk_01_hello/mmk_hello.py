11a07703ed26483c22cd2212105912523a7d19d0
Arjibsamlee <arjibsamlee@yahoo.com>
2021-08-30 10:50:07 -0700

    add test

--- mmk_be534/mmk_01_hello/mmk_hello.py
+++ mmk_be534/mmk_01_hello/mmk_hello.py
@@ -1,7 +1,32 @@
-#! /usr/bin/env pthon3
+#!/usr/bin/env pthon3  
 
-#   Author:     Megan Kane
-#   Class:      BE 534
-#   Purpose:    Say Hello
+"""
+Author:     Megan Kane
+Class:      BE 534
+Purpose:    Say Hello
+"""
+#   call functions
 
-print ("Hello World!") # say hello to the world
+import argparse
+
+#-------------------------------
+
+def get_args():
+    # pull te command line arguments
+
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
+#-------------------------------
+
+def main():
+    # define the main program
+
+    args = get_args()
+    print("Hello, " + args.name + '!')
+
+#-------------------------------
+if __name__ == '__main__':
+    main()
+
--- mmk_be534/mmk_01_hello/mmk_test_hello.py
+++ mmk_be534/mmk_01_hello/mmk_test_hello.py
@@ -2,6 +2,50 @@
 """
 Tests for mmk_hello.py
 """
+import os
+from subprocess import getoutput, getstatusoutput
+
 
 prg = './mmk_hello.py'
+
+
+#---------------------
+
+def test_exists():
+    # Does the file exixt?
+
+    assert os.path.isfile(prg)
+
+#----------------------
+def test_runnable():
+    # test runs on python3
+    out = getoutput(f'python3 {prg}')
+    assert out.strip() == 'Hello, World!'
+
+#-----------------------
+def test_executable():
+    # Says Hello Wolrd! by default
+    out = getoutput(prg)
+    assert out.strip() == 'Hello, World!'
+
+
+#-------------------------
+def test_usage():
+    # the usage of the program
+    for flag in ['-h', '--help']:
+        rv, out = getstatusoutput(f'{prg}{flag}')
+        assert rv == 0
+        assert out.lower().startswith('ussage')
+
+#-------------------------------
+
+def test_input():
+    #test for input
+    for val in ['Universe', 'Multiverse']:
+        for option in ['-n', '--name']:
+            rv, out = getstatusupdate(f'{prg} {option} {val}')
+            assert rv == 0
+            assert out.strip() == f'Hello, {val}!'
+
+# end program
 
