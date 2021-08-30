d1a55444cf02d6a12538ef041548f637abbbafe7
Arjibsamlee <arjibsamlee@yahoo.com>
2021-08-30 09:34:37 -0700

    updated

--- mmk_be534/mmk_01_hello/mmk_hello.py
+++ mmk_be534/mmk_01_hello/mmk_hello.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env pthon3
-""" 
+    """ 
     Author: Megan Kane
     Class:  BE 534
     Purpose:Say Hello
     """
 
-print ("Hello World")
+print ("Hello World!") # say hello to the world
