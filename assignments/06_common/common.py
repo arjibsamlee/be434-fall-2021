4e1b29ff99a95d75fbd08991588fbdccb8178059
Arjibsamlee <arjibsamlee@yahoo.com>
2021-10-13 08:25:51 -0700

    need assistance

---
+++ assignments/06_common/common.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""
+Author : Megan Kane <arjibsamlee@yahoo.com>
+Date   : 2021-10-13
+Purpose: look for commmon words between files
+"""
+
+import argparse
+import sys
+
+# pylint: disable=W0105
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='look for commmon words between files',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file1',
+                        metavar='FILE1',
+                        type=argparse.FileType('rt'),
+                        help='Input file 1')
+
+    parser.add_argument('file2',
+                        metavar='FILE2',
+                        type=argparse.FileType('rt'),
+                        help='Input file 1')
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='A readable file',
+                        metavar='FILE',
+                        type=argparse.FileType('wt'),
+                        default=None)
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    onefile = args.file1
+    twofile = args.file2
+    output = args.outfile
+
+
+    # print('File 1 = "{}"'.format(onefile.name if onefile else ''))
+    # print('File 2 = "{}"'.format(twofile.name if twofile else ''))
+
+    oneset = set()
+    twoset = set()
+
+    for word in onefile.read().split():
+        oneset.add(word)
+
+    for word in twofile.read().split():
+        twoset.add(word)
+
+    # print(oneset)
+    # print(twoset)
+
+    common = oneset.intersection(twoset)
+
+    # print("words that are in common: ",common)
+
+    if args.outfile:
+        output.write(common)
+    else:
+        print('{}'.format(common, set='/n'))
+
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
