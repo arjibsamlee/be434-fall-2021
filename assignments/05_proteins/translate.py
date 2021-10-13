bb2a0b8d125b9c2f4b4ccedb41394fdf0141c81f
Arjibsamlee <arjibsamlee@yahoo.com>
2021-10-13 07:31:38 -0700

    changes done in class

--- assignments/05_proteins/translate.py
+++ assignments/05_proteins/translate.py
@@ -6,6 +6,7 @@ Purpose: translate a given DNA/RNA sequence to amino acids
 """
 
 import argparse
+# from pprint import pprint
 
 
 # pylint: disable=W0105
@@ -19,7 +20,7 @@ def get_args():
         description='translate a given DNA/RNA sequence to amino acids',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('protein',
+    parser.add_argument('sequence',
                         metavar='str',
                         help='DNA/RNA sequence')
 
@@ -46,80 +47,28 @@ def main():
     """Start of program"""
     # assign
     args = get_args()
-    protein_in = args.protein
-    codon = args.codons
-    output = args.outfile
+    seq = args.sequence
+    # codon = args.codons
+    # output = args.outfile
 
-    testfile = temp_file(codon)
-    # file_length = file_lines(testfile)
+    # print(args)
 
-    sample = ''
+    codon_table = {}
+    for line in args.codons:
+        key, val = line.split()
+        codon_table[key] = val
 
-    # print(f'protein_in = "{protein_in}"')
-    # print('codonfile = "{}"'.format(codon.name if codon else ''))
-    # print('file_arg = "{}"'.format(output.name if output else ''))
-    # print('the codon file is', file_length, 'lines long.')
-    # print('The first 3 digits of the input are:', get3(protein_in,0), '')
+    # pprint(codon_table)
 
-    j = 0
-    # counter for the section of the DNA/RNA sequence
+    k = 3
+    protein = ''
 
-    while j < len(protein_in):
-        sample = get3(protein_in, j)
-        sample = sample.upper()
-        # print(infile_check(testfile,sample))
-        output.write(infile_check(testfile, sample, 0, 3))
+    for codon in [seq[i:i+k] for i in range(0, len(seq) - k + 1)]:
+        # print(codon_table.get(codon.upper(), '-'), end='')
+        protein += codon_table.get(codon.upper(), '-')
 
-        j += 3
-    # final_out = output.read()
-    print(f'Output written to "{output.name}".')
-
-
-# ---------------------------------------------------
-def temp_file(filename):
-    """creates a temp file copy to work with without editing original"""
-    with open('temp.txt', 'wt', encoding='utf-8') as temp:
-        for i in filename:
-            temp.write(i)
-        # print('in temp file test')
-
-    return 'temp.txt'
-
-
-# ---------------------------------------------------
-def get3(test_str, num_start):
-    """get 3 digits of a string"""
-    twa = ''
-    num_end = num_start+3
-    twa = test_str[num_start:num_end]
-
-    return twa
-
-
-# ---------------------------------------------------
-def file_lines(filename):
-    """get number of lines"""
-    num_lines, num_words, num_bytes = 0, 0, 0
-    for line in filename:
-        num_lines += 1
-        num_bytes += len(line)
-        num_words += len(line.split())
-
-    return num_lines
-
-
-# ---------------------------------------------------
-def infile_check(filename, testfile, num_s, num_e):
-    """See if a testfile is in any line of a file"""
-    with open(filename, 'rt', encoding='utf-8') as x:
-        for i in x:
-            # print(i)
-            sample_test = i[num_s:num_e]
-            # print(sample_test)
-            if testfile == sample_test:
-                return i[4]
-
-    return '-'
+    print(protein, file=args.outfile)
+    print(f'Output written to "{args.outfile.name}".')
 
 
 # --------------------------------------------------
