0e9c354988968c607ea346fe677bfd3c840f9f2d
Arjibsamlee <arjibsamlee@yahoo.com>
2021-10-19 11:04:21 -0700

    initial program assignemnet 7

---
+++ assignments/07_iupac/iupac.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+"""
+Author : Megan Kane <arjibsamlee@yahoo.com>
+Date   : 2021-10-19
+Purpose: Translate IUPAC encoded DNA to regular expression
+"""
+
+import argparse
+import sys
+import os
+# from pprint import pprint
+
+# pylint: disable=W0105
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Translate IUPAC encoded DNA to regular expression',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('sequence',
+                        metavar='SEQ',
+                        nargs= '+',
+                        type=str,
+                        help='A DNA sequence')
+
+    parser.add_argument('-o',
+                            '--outfile',
+                            help='Location for output',
+                            metavar='FILE',
+                            type=argparse.FileType('wt'),
+                            default=[sys.stdout])
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    seq = args.sequence
+    # out_fh = args.outfile
+    is_file = 'no'  if args.outfile == [sys.stdout] else 'yes'
+    # print(args)
+    # print(is_file)
+
+    iupac_table = {}
+    iupac_codes = open('iupac_codes.txt')
+
+    for line in iupac_codes:
+        key, val = line.split()
+        iupac_table[key] = val
+
+    # pprint(iupac_table)
+
+    for index in range(len(seq)):
+        protein = ''
+        seq_now = seq[index]
+        # print(seq_now)
+
+        for i in seq_now:
+           # print('in for loop')
+            compare = iupac_table.get(i.upper())
+
+
+           # print('compare this ' + i + ' to ' + compare)
+
+            if i == compare:
+                protein += compare
+                # print('in if')
+            else:
+                protein += '[' + compare + ']'
+                # print('in else')
+        # print(seq_now + ' ' + protein)
+
+        if is_file == 'yes':
+           print(seq_now + ' ' + protein, file=args.outfile)
+
+        else:
+            print(seq_now + ' ' + protein)
+
+    if is_file == 'yes':
+        print(f'Done, see output in "{args.outfile.name}".')
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
